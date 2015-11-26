07e2ef567f55da2b9ddfaa5d980d2f51c94ac04e
lintool <jimmylin@uwaterloo.ca>
2015-11-26 13:02:17 -0500

    Bigram count in both MapReduce and Spark: generates same output.

---
+++ src/main/java/io/bespin/demo/mapreduce/bigram/BigramCount.java
@@ -0,0 +1,145 @@
+package io.bespin.demo.mapreduce.bigram;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
+
+/**
+ * Counts bigrams.
+ */
+public class BigramCount extends Configured implements Tool {
+  private static final Logger LOG = Logger.getLogger(BigramCount.class);
+
+  protected static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
+    private static final IntWritable ONE = new IntWritable(1);
+    private static final Text BIGRAM = new Text();
+
+    @Override
+    public void map(LongWritable key, Text value, Context context)
+        throws IOException, InterruptedException {
+      String line = value.toString();
+
+      String prev = null;
+      StringTokenizer itr = new StringTokenizer(line);
+      while (itr.hasMoreTokens()) {
+        String cur = itr.nextToken();
+
+        // Emit only if we have an actual bigram.
+        if (prev != null) {
+          BIGRAM.set(prev + " " + cur);
+          context.write(BIGRAM, ONE);
+        }
+        prev = cur;
+      }
+    }
+  }
+
+  protected static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    private final static IntWritable SUM = new IntWritable();
+
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context) throws IOException,
+        InterruptedException {
+      int sum = 0;
+      Iterator<IntWritable> iter = values.iterator();
+      while (iter.hasNext()) {
+        sum += iter.next().get();
+      }
+      SUM.set(sum);
+      context.write(key, SUM);
+    }
+  }
+
+  /**
+   * Creates an instance of this tool.
+   */
+  public BigramCount() {}
+
+  public static class Args {
+    @Option(name = "-input", metaVar = "[path]", required = true, usage = "input path")
+    public String input;
+
+    @Option(name = "-output", metaVar = "[path]", required = true, usage = "output path")
+    public String output;
+
+    @Option(name = "-reducers", metaVar = "[num]", required = false, usage = "number of reducers")
+    public int numReducers = 1;
+  }
+
+  /**
+   * Runs this tool.
+   */
+  public int run(String[] argv) throws Exception {
+    Args args = new Args();
+    CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(100));
+
+    try {
+      parser.parseArgument(argv);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      return -1;
+    }
+
+    LOG.info("Tool name: " + BigramCount.class.getSimpleName());
+    LOG.info(" - input path: " + args.input);
+    LOG.info(" - output path: " + args.output);
+    LOG.info(" - num reducers: " + args.numReducers);
+
+    Job job = Job.getInstance(getConf());
+    job.setJobName(BigramCount.class.getSimpleName());
+    job.setJarByClass(BigramCount.class);
+
+    job.setNumReduceTasks(args.numReducers);
+
+    FileInputFormat.setInputPaths(job, new Path(args.input));
+    FileOutputFormat.setOutputPath(job, new Path(args.output));
+
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(IntWritable.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+
+    job.setMapperClass(MyMapper.class);
+    job.setCombinerClass(MyReducer.class);
+    job.setReducerClass(MyReducer.class);
+
+    // Delete the output directory if it exists already.
+    Path outputDir = new Path(args.output);
+    FileSystem.get(getConf()).delete(outputDir, true);
+
+    long startTime = System.currentTimeMillis();
+    job.waitForCompletion(true);
+    System.out.println("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
+
+    return 0;
+  }
+
+  /**
+   * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
+   */
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new BigramCount(), args);
+  }
+}
---
+++ src/main/scala/io/bespin/demo/spark/bigram/BigramCount.scala
@@ -0,0 +1,33 @@
+package io.bespin.demo.spark.bigram;
+
+import java.util.StringTokenizer
+
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkConf
+
+import scala.collection.JavaConversions._
+
+object BigramCount {
+  val usage = """
+    Usage: spark-submit --class io.bespin.demo.spark.bigram.BigramCount target/bespin.jar [input] [output]
+  """
+
+  def main(args: Array[String]) {
+    if (args.length == 0) {
+      println(usage)
+      System.exit(-1);
+    }
+
+    val conf = new SparkConf().setAppName("Word Count")
+    val sc = new SparkContext(conf)
+    val textFile = sc.textFile(args(0))
+    val counts = textFile
+      .flatMap(line => {
+        val tokens = new StringTokenizer(line).toList
+        if (tokens.length>1) tokens.sliding(2).map(p => p.mkString(" ")).toList else List()
+      })
+      .map(bigram => (bigram, 1))
+      .reduceByKey(_ + _)
+    counts.saveAsTextFile(args(1))
+  }
+}
