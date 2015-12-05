bbdabc0cb99e5c91455daedf71a3ed4fd8fb3184
lintool <jimmylin@uwaterloo.ca>
2015-12-05 13:46:23 -0500

    Switched from using JavaConversions to JavaConverters.

--- src/main/scala/io/bespin/scala/util/Tokenizer.scala
+++ src/main/scala/io/bespin/scala/util/Tokenizer.scala
@@ -2,11 +2,11 @@ package io.bespin.scala.util;
 
 import java.util.StringTokenizer
 
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
 
 trait Tokenizer {
   def tokenize(s: String): List[String] = {
-    new StringTokenizer(s).toList
+    new StringTokenizer(s).asScala.toList
       .map(_.asInstanceOf[String].toLowerCase().replaceAll("(^[^a-z]+|[^a-z]+$)", ""))
       .filter(_.length != 0)
   }
