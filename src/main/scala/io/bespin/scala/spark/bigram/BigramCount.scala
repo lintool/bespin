9b3dfb90161c39de16fe5a2d88764882a20498a9
lintool <jimmylin@uwaterloo.ca>
2015-12-06 14:10:49 -0500

    Refactored Spark bigram count; Added Scala MapReduce bigram count.

--- src/main/scala/io/bespin/scala/spark/bigram/BigramCount.scala
+++ src/main/scala/io/bespin/scala/spark/bigram/BigramCount.scala
@@ -1,26 +1,37 @@
-package io.bespin.scala.spark.bigram;
+package io.bespin.scala.spark.bigram
 
 import io.bespin.scala.util.Tokenizer
 
+import org.apache.log4j._
+import org.apache.hadoop.fs._
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
+import org.rogach.scallop._
 
-import scala.collection.JavaConversions._
+class Conf(args: Seq[String]) extends ScallopConf(args) with Tokenizer {
+  mainOptions = Seq(input, output, reducers)
+  val input = opt[String](descr = "input path", required = true)
+  val output = opt[String](descr = "output path", required = true)
+  val reducers = opt[Int](descr = "number of reducers", required = false, default = Some(1))
+}
 
 object BigramCount extends Tokenizer {
-  val usage = """
-    Usage: spark-submit --class io.bespin.scala.spark.bigram.BigramCount target/bespin.jar [input] [output]
-  """
+  val log = Logger.getLogger(getClass().getName());
 
-  def main(args: Array[String]) {
-    if (args.length == 0) {
-      println(usage)
-      System.exit(-1);
-    }
+  def main(argv: Array[String]) {
+    val args = new Conf(argv)
 
-    val conf = new SparkConf().setAppName("Word Count")
+    log.info("Input: " + args.input())
+    log.info("Output: " + args.output())
+    log.info("Number of reducers: " + args.reducers())
+
+    val conf = new SparkConf().setAppName("Bigram Count")
     val sc = new SparkContext(conf)
-    val textFile = sc.textFile(args(0))
+
+    val outputDir = new Path(args.output());
+    FileSystem.get(sc.hadoopConfiguration).delete(outputDir, true);
+
+    val textFile = sc.textFile(args.input())
     val counts = textFile
       .flatMap(line => {
         val tokens = tokenize(line)
@@ -28,6 +39,6 @@ object BigramCount extends Tokenizer {
       })
       .map(bigram => (bigram, 1))
       .reduceByKey(_ + _)
-    counts.saveAsTextFile(args(1))
+    counts.saveAsTextFile(args.output())
   }
 }
