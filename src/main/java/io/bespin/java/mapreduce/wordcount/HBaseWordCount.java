584132757177b93b2e3eab4625f04c8065546129
lintool <jimmylin@uwaterloo.ca>
2016-03-14 08:09:39 -0400

    Refactored HBase word count example.

--- src/main/java/io/bespin/java/mapreduce/wordcount/HBaseWordCount.java
+++ src/main/java/io/bespin/java/mapreduce/wordcount/HBaseWordCount.java
@@ -1,17 +1,7 @@
 package io.bespin.java.mapreduce.wordcount;
 
 import java.io.IOException;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.StringTokenizer;
 
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.GnuParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.OptionBuilder;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
@@ -26,15 +16,16 @@ import org.apache.hadoop.hbase.mapreduce.TableMapReduceUtil;
 import org.apache.hadoop.hbase.mapreduce.TableReducer;
 import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.Reducer;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
 
 /**
  * Simple word count demo using HBase for storage.
@@ -45,45 +36,6 @@ public class HBaseWordCount extends Configured implements Tool {
   public static final String[] FAMILIES = { "c" };
   public static final byte[] CF = FAMILIES[0].getBytes();
   public static final byte[] COUNT = "count".getBytes();
-
-  // Mapper: emits (token, 1) for every word occurrence.
-  private static class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-
-    // Reuse objects to save overhead of object creation.
-    private final static IntWritable ONE = new IntWritable(1);
-    private final static Text WORD = new Text();
-
-    @Override
-    public void map(LongWritable key, Text value, Context context)
-        throws IOException, InterruptedException {
-      String line = ((Text) value).toString();
-      StringTokenizer itr = new StringTokenizer(line);
-      while (itr.hasMoreTokens()) {
-        WORD.set(itr.nextToken());
-        context.write(WORD, ONE);
-      }
-    }
-  }
-
-  // Reducer: sums up all the counts.
-  private static class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
-
-    // Reuse objects.
-    private final static IntWritable SUM = new IntWritable();
-
-    @Override
-    public void reduce(Text key, Iterable<IntWritable> values, Context context)
-        throws IOException, InterruptedException {
-      // Sum up values.
-      Iterator<IntWritable> iter = values.iterator();
-      int sum = 0;
-      while (iter.hasNext()) {
-        sum += iter.next().get();
-      }
-      SUM.set(sum);
-      context.write(key, SUM);
-    }
-  }
 
   public static class MyTableReducer extends TableReducer<Text, IntWritable, ImmutableBytesWritable>  {
     public void reduce(Text key, Iterable<IntWritable> values, Context context)
@@ -104,79 +56,67 @@ public class HBaseWordCount extends Configured implements Tool {
    */
   public HBaseWordCount() {}
 
-  private static final String INPUT = "input";
-  private static final String OUTPUT = "output";
-  private static final String NUM_REDUCERS = "numReducers";
+  public static class Args {
+    @Option(name = "-input", metaVar = "[path]", required = true, usage = "input path")
+    public String input;
+
+    @Option(name = "-table", metaVar = "[name]", required = true, usage = "output path")
+    public String table;
+
+    @Option(name = "-config", metaVar = "[path]", required = true, usage = "HBase config")
+    public String config;
+
+    @Option(name = "-reducers", metaVar = "[num]", required = false, usage = "number of reducers")
+    public int numReducers = 1;
+  }
 
   /**
    * Runs this tool.
    */
-  @SuppressWarnings({ "static-access" })
-  public int run(String[] args) throws Exception {
-    Options options = new Options();
-
-    options.addOption(OptionBuilder.withArgName("path").hasArg()
-        .withDescription("input path").create(INPUT));
-    options.addOption(OptionBuilder.withArgName("table").hasArg()
-        .withDescription("HBase table name").create(OUTPUT));
-    options.addOption(OptionBuilder.withArgName("num").hasArg()
-        .withDescription("number of reducers").create(NUM_REDUCERS));
-
-    CommandLine cmdline;
-    CommandLineParser parser = new GnuParser();
+  public int run(String[] argv) throws Exception {
+    Args args = new Args();
+    CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(100));
 
     try {
-      cmdline = parser.parse(options, args);
-    } catch (ParseException exp) {
-      System.err.println("Error parsing command line: " + exp.getMessage());
+      parser.parseArgument(argv);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
       return -1;
     }
 
-    if (!cmdline.hasOption(INPUT) || !cmdline.hasOption(OUTPUT)) {
-      System.out.println("args: " + Arrays.toString(args));
-      HelpFormatter formatter = new HelpFormatter();
-      formatter.setWidth(120);
-      formatter.printHelp(this.getClass().getName(), options);
-      ToolRunner.printGenericCommandUsage(System.out);
-      return -1;
-    }
-
-    String inputPath = cmdline.getOptionValue(INPUT);
-    String outputTable = cmdline.getOptionValue(OUTPUT);
-    int reduceTasks = cmdline.hasOption(NUM_REDUCERS) ?
-        Integer.parseInt(cmdline.getOptionValue(NUM_REDUCERS)) : 1;
+    LOG.info("Tool: " + HBaseWordCount.class.getSimpleName());
+    LOG.info(" - input path: " + args.input);
+    LOG.info(" - output table: " + args.table);
+    LOG.info(" - config: " + args.config);
+    LOG.info(" - number of reducers: " + args.numReducers);
 
     // If the table doesn't already exist, create it.
     Configuration conf = getConf();
-    conf.addResource(new Path("/etc/hbase/conf/hbase-site.xml"));
+    conf.addResource(new Path(args.config));
 
     Configuration hbaseConfig = HBaseConfiguration.create(conf);
     HBaseAdmin admin = new HBaseAdmin(hbaseConfig);
 
-    if (admin.tableExists(outputTable)) {
-      LOG.info(String.format("Table '%s' exists: dropping table and recreating.", outputTable));
-      LOG.info(String.format("Disabling table '%s'", outputTable));
-      admin.disableTable(outputTable);
-      LOG.info(String.format("Droppping table '%s'", outputTable));
-      admin.deleteTable(outputTable);
+    if (admin.tableExists(args.table)) {
+      LOG.info(String.format("Table '%s' exists: dropping table and recreating.", args.table));
+      LOG.info(String.format("Disabling table '%s'", args.table));
+      admin.disableTable(args.table);
+      LOG.info(String.format("Droppping table '%s'", args.table));
+      admin.deleteTable(args.table);
     }
 
-    HTableDescriptor tableDesc = new HTableDescriptor(TableName.valueOf(outputTable));
+    HTableDescriptor tableDesc = new HTableDescriptor(TableName.valueOf(args.table));
     for (int i = 0; i < FAMILIES.length; i++) {
       HColumnDescriptor hColumnDesc = new HColumnDescriptor(FAMILIES[i]);
       tableDesc.addFamily(hColumnDesc);
     }
     admin.createTable(tableDesc);
-    LOG.info(String.format("Successfully created table '%s'", outputTable));
+    LOG.info(String.format("Successfully created table '%s'", args.table));
 
     admin.close();
 
     // Now we're ready to start running MapReduce.
-    LOG.info("Tool: " + HBaseWordCount.class.getSimpleName());
-    LOG.info(" - input path: " + inputPath);
-    LOG.info(" - output table: " + outputTable);
-    LOG.info(" - number of reducers: " + reduceTasks);
-
     Job job = Job.getInstance(conf);
     job.setJobName(HBaseWordCount.class.getSimpleName());
     job.setJarByClass(HBaseWordCount.class);
@@ -184,12 +124,12 @@ public class HBaseWordCount extends Configured implements Tool {
     job.setMapOutputKeyClass(Text.class);
     job.setMapOutputValueClass(IntWritable.class);
 
-    job.setMapperClass(MyMapper.class);
-    job.setCombinerClass(MyReducer.class);
-    job.setNumReduceTasks(reduceTasks);
+    job.setMapperClass(WordCount.MyMapper.class);
+    job.setCombinerClass(WordCount.MyReducer.class);
+    job.setNumReduceTasks(args.numReducers);
 
-    FileInputFormat.setInputPaths(job, new Path(inputPath));
-    TableMapReduceUtil.initTableReducerJob(outputTable, MyTableReducer.class, job);
+    FileInputFormat.setInputPaths(job, new Path(args.input));
+    TableMapReduceUtil.initTableReducerJob(args.table, MyTableReducer.class, job);
 
     long startTime = System.currentTimeMillis();
     job.waitForCompletion(true);
--- src/main/java/io/bespin/java/mapreduce/wordcount/HBaseWordCountFetch.java
+++ src/main/java/io/bespin/java/mapreduce/wordcount/HBaseWordCountFetch.java
@@ -1,14 +1,5 @@
 package io.bespin.java.mapreduce.wordcount;
 
-import java.util.Arrays;
-
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.CommandLineParser;
-import org.apache.commons.cli.GnuParser;
-import org.apache.commons.cli.HelpFormatter;
-import org.apache.commons.cli.OptionBuilder;
-import org.apache.commons.cli.Options;
-import org.apache.commons.cli.ParseException;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
@@ -22,11 +13,13 @@ import org.apache.hadoop.hbase.util.Bytes;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 import org.apache.log4j.Logger;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
 
 /**
  * Simple demo of HBase; fetching word counts.
- *
- * @author Jimmy Lin
  */
 public class HBaseWordCountFetch extends Configured implements Tool {
   private static final Logger LOG = Logger.getLogger(HBaseWordCountFetch.class);
@@ -36,56 +29,45 @@ public class HBaseWordCountFetch extends Configured implements Tool {
    */
   public HBaseWordCountFetch() {}
 
-  private static final String TABLE = "table";
-  private static final String WORD = "word";
+  public static class Args {
+    @Option(name = "-term", metaVar = "[term]", required = true, usage = "term to look up")
+    public String term;
+
+    @Option(name = "-table", metaVar = "[name]", required = true, usage = "output path")
+    public String table;
+
+    @Option(name = "-config", metaVar = "[path]", required = true, usage = "HBase config")
+    public String config;
+  }
 
   /**
    * Runs this tool.
    */
-  @SuppressWarnings({ "static-access" })
-  public int run(String[] args) throws Exception {
-    Options options = new Options();
-
-    options.addOption(OptionBuilder.withArgName("table").hasArg()
-        .withDescription("HBase table name").create(TABLE));
-    options.addOption(OptionBuilder.withArgName("word").hasArg()
-        .withDescription("word to look up").create(WORD));
-
-    CommandLine cmdline;
-    CommandLineParser parser = new GnuParser();
+  public int run(String[] argv) throws Exception {
+    Args args = new Args();
+    CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(100));
 
     try {
-      cmdline = parser.parse(options, args);
-    } catch (ParseException exp) {
-      System.err.println("Error parsing command line: " + exp.getMessage());
+      parser.parseArgument(argv);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
       return -1;
     }
-
-    if (!cmdline.hasOption(TABLE) || !cmdline.hasOption(WORD)) {
-      System.out.println("args: " + Arrays.toString(args));
-      HelpFormatter formatter = new HelpFormatter();
-      formatter.setWidth(120);
-      formatter.printHelp(this.getClass().getName(), options);
-      ToolRunner.printGenericCommandUsage(System.out);
-      return -1;
-    }
-
-    String tableName = cmdline.getOptionValue(TABLE);
-    String word = cmdline.getOptionValue(WORD);
 
     Configuration conf = getConf();
-    conf.addResource(new Path("/etc/hbase/conf/hbase-site.xml"));
+    conf.addResource(new Path("/home/jimmylin/hbase-0.98.16-hadoop2/conf/hbase-site.xml"));
 
     Configuration hbaseConfig = HBaseConfiguration.create(conf);
     HConnection hbaseConnection = HConnectionManager.createConnection(hbaseConfig);
-    HTableInterface table = hbaseConnection.getTable(tableName);
+    HTableInterface table = hbaseConnection.getTable(args.table);
 
-    Get get = new Get(Bytes.toBytes(word));
+    Get get = new Get(Bytes.toBytes(args.term));
     Result result = table.get(get);
 
     int count = Bytes.toInt(result.getValue(HBaseWordCount.CF, HBaseWordCount.COUNT));
 
-    LOG.info("word: " + word + ", count: " + count);
+    LOG.info("term: " + args.term + ", count: " + count);
 
     return 0;
   }
