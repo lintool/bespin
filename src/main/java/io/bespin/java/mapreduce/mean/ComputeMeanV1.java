1cc4cbe887a2563267d076c672ac237870726aef
lintool <jimmylin@uwaterloo.ca>
2017-01-05 10:45:23 -0500

    Different versions of algorithms for computing the mean.

--- src/main/java/io/bespin/java/mapreduce/mean/ComputeMeanV1.java
+++ src/main/java/io/bespin/java/mapreduce/mean/ComputeMeanV1.java
@@ -16,18 +16,17 @@
 
 package io.bespin.java.mapreduce.mean;
 
-import org.apache.commons.lang.RandomStringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
-import org.apache.hadoop.fs.FSDataOutputStream;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
-import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
-import org.apache.hadoop.mapreduce.lib.input.NLineInputFormat;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import org.apache.hadoop.util.Tool;
@@ -39,56 +38,51 @@ import org.kohsuke.args4j.Option;
 import org.kohsuke.args4j.ParserProperties;
 
 import java.io.IOException;
-import java.util.Random;
+import java.util.Iterator;
 
 /**
- * Simple word count demo.
+ * Compute the mean of values of a key (version 1).
  */
-public class GenerateRandomData extends Configured implements Tool {
-  private static final Logger LOG = Logger.getLogger(GenerateRandomData.class);
+public class ComputeMeanV1 extends Configured implements Tool {
+  private static final Logger LOG = Logger.getLogger(ComputeMeanV1.class);
 
-  private static final class MyMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-    private final Random random = new Random(System.nanoTime());
-    private int keyLength = 1;
-    private int valueRange = Integer.MAX_VALUE;
-
+  private static final class MyMapper extends Mapper<Text, Text, Text, IntWritable> {
     @Override
-    public void setup(Context context) {
-      keyLength = context.getConfiguration().getInt("keyLength", 1);
-      valueRange = context.getConfiguration().getInt("valueRange", Integer.MAX_VALUE);
-    }
-
-    @Override
-    public void map(LongWritable key, Text value, Context context)
+    public void map(Text key, Text value, Context context)
         throws IOException, InterruptedException {
-      int n = Integer.parseInt(value.toString());
-      for (int i = 0; i < n; i++) {
-        context.write(new Text(RandomStringUtils.randomAlphabetic(keyLength)),
-            new IntWritable(random.nextInt(valueRange)));
+      context.write(key, new IntWritable(Integer.parseInt(value.toString())));
+    }
+  }
+
+  private static final class MyReducer extends Reducer<Text, IntWritable, Text, IntWritable> {
+    @Override
+    public void reduce(Text key, Iterable<IntWritable> values, Context context)
+        throws IOException, InterruptedException {
+      Iterator<IntWritable> iter = values.iterator();
+      int sum = 0;
+      int cnt = 0;
+      while (iter.hasNext()) {
+        sum += iter.next().get();
+        cnt++;
       }
+      context.write(key, new IntWritable(sum/cnt));
     }
   }
 
   /**
    * Creates an instance of this tool.
    */
-  private GenerateRandomData() {}
+  private ComputeMeanV1() {}
 
   private static final class Args {
-    @Option(name = "-mappers", metaVar = "[num]", usage = "number of mappers")
-    int mappers = 10;
-
-    @Option(name = "-num", metaVar = "[num]", usage = "number of random numbers per mapper")
-    int num = 100;
-
-    @Option(name = "-keyLength", metaVar = "[num]", usage = "length of each key")
-    int keyLength = 1;
-
-    @Option(name = "-valueRange", metaVar = "[num]", usage = "range of each value")
-    int valueRange = Integer.MAX_VALUE;
+    @Option(name = "-input", metaVar = "[path]", required = true, usage = "input path")
+    String input;
 
     @Option(name = "-output", metaVar = "[path]", required = true, usage = "output path")
     String output;
+
+    @Option(name = "-reducers", metaVar = "[num]", usage = "number of reducers")
+    int numReducers = 1;
   }
 
   /**
@@ -107,52 +101,38 @@ public class GenerateRandomData extends Configured implements Tool {
       return -1;
     }
 
-    LOG.info("Tool: " + GenerateRandomData.class.getSimpleName());
-    LOG.info(" - number of mappers: " + args.mappers);
-    LOG.info(" - random numbers per mapper: " + args.num);
-    LOG.info(" - key length: " + args.keyLength);
-    LOG.info(" - value range: " + args.valueRange);
+    LOG.info("Tool: " + ComputeMeanV1.class.getSimpleName());
+    LOG.info(" - input path: " + args.input);
     LOG.info(" - output path: " + args.output);
+    LOG.info(" - number of reducers: " + args.numReducers);
 
     Configuration conf = getConf();
-    Random rand = new Random();
-
-    Path tmp = new Path("tmp" + rand.nextInt(1000) + ".txt");
-    FileSystem fs = FileSystem.get(conf);
-    FSDataOutputStream dataOut = fs.create(tmp);
-    for (int i=0; i<args.mappers; i++) {
-      dataOut.write((args.num + "\n").getBytes());
-    }
-    dataOut.close();
-
     Job job = Job.getInstance(conf);
-    job.setJobName(GenerateRandomData.class.getSimpleName());
-    job.setJarByClass(GenerateRandomData.class);
+    job.setJobName(ComputeMeanV1.class.getSimpleName());
+    job.setJarByClass(ComputeMeanV1.class);
 
-    job.getConfiguration().setInt("keyLength", args.keyLength);
-    job.getConfiguration().setInt("valueRange", args.valueRange);
+    job.setNumReduceTasks(args.numReducers);
 
-    job.setNumReduceTasks(0);
-
-    job.setInputFormatClass(NLineInputFormat.class);
-    job.getConfiguration().setInt("mapreduce.input.lineinputformat.linespermap", 1);
-    NLineInputFormat.addInputPath(job, tmp);
-
-    job.setOutputFormatClass(TextOutputFormat.class);
+    FileInputFormat.setInputPaths(job, new Path(args.input));
     FileOutputFormat.setOutputPath(job, new Path(args.output));
 
+    job.setInputFormatClass(KeyValueTextInputFormat.class);
+    job.getConfiguration().set("mapreduce.input.keyvaluelinerecordreader.key.value.separator", "\t");
     job.setMapOutputKeyClass(Text.class);
     job.setMapOutputValueClass(IntWritable.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
 
     job.setMapperClass(MyMapper.class);
+    job.setReducerClass(MyReducer.class);
 
     // Delete the output directory if it exists already.
     Path outputDir = new Path(args.output);
-    fs.delete(outputDir, true);
+    FileSystem.get(conf).delete(outputDir, true);
 
     long startTime = System.currentTimeMillis();
     job.waitForCompletion(true);
-    fs.delete(tmp, true);
     LOG.info("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
 
     return 0;
@@ -162,6 +142,6 @@ public class GenerateRandomData extends Configured implements Tool {
    * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
    */
   public static void main(String[] args) throws Exception {
-    ToolRunner.run(new GenerateRandomData(), args);
+    ToolRunner.run(new ComputeMeanV1(), args);
   }
 }
---
+++ src/main/java/io/bespin/java/mapreduce/mean/ComputeMeanV4.java
@@ -0,0 +1,180 @@
+/**
+ * Bespin: reference implementations of "big data" algorithms
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.bespin.java.mapreduce.mean;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
+import org.apache.log4j.Logger;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+import org.kohsuke.args4j.Option;
+import org.kohsuke.args4j.ParserProperties;
+import tl.lin.data.map.HMapStIW;
+import tl.lin.data.pair.PairOfInts;
+
+import java.io.IOException;
+import java.util.Iterator;
+
+/**
+ * Compute the mean of values of a key (version 4).
+ */
+public class ComputeMeanV4 extends Configured implements Tool {
+  private static final Logger LOG = Logger.getLogger(ComputeMeanV4.class);
+
+  private static final class MyMapper extends Mapper<Text, Text, Text, PairOfInts> {
+    private static final HMapStIW SUMS = new HMapStIW();
+    private static final HMapStIW COUNTS = new HMapStIW();
+
+    @Override
+    public void map(Text key, Text value, Context context)
+        throws IOException, InterruptedException {
+      String k = key.toString();
+      SUMS.put(k, SUMS.get(k) + Integer.parseInt(value.toString()));
+      COUNTS.put(k, COUNTS.get(k) + 1);
+    }
+
+    @Override
+    public void cleanup(Context context)
+        throws IOException, InterruptedException {
+      for (String key : SUMS.keySet()) {
+        context.write(new Text(key), new PairOfInts(SUMS.get(key), COUNTS.get(key)));
+      }
+    }
+  }
+
+  private static final class MyCombiner extends Reducer<Text, PairOfInts, Text, PairOfInts> {
+    @Override
+    public void reduce(Text key, Iterable<PairOfInts> values, Context context)
+        throws IOException, InterruptedException {
+      Iterator<PairOfInts> iter = values.iterator();
+      int sum = 0;
+      int cnt = 0;
+      while (iter.hasNext()) {
+        PairOfInts pair = iter.next();
+        sum += pair.getLeftElement();
+        cnt += pair.getRightElement();
+      }
+      context.write(key, new PairOfInts(sum, cnt));
+    }
+  }
+
+  private static final class MyReducer extends Reducer<Text, PairOfInts, Text, IntWritable> {
+    @Override
+    public void reduce(Text key, Iterable<PairOfInts> values, Context context)
+        throws IOException, InterruptedException {
+      Iterator<PairOfInts> iter = values.iterator();
+      int sum = 0;
+      int cnt = 0;
+      while (iter.hasNext()) {
+        PairOfInts pair = iter.next();
+        sum += pair.getLeftElement();
+        cnt += pair.getRightElement();
+      }
+      context.write(key, new IntWritable(sum/cnt));
+    }
+  }
+
+  /**
+   * Creates an instance of this tool.
+   */
+  private ComputeMeanV4() {}
+
+  private static final class Args {
+    @Option(name = "-input", metaVar = "[path]", required = true, usage = "input path")
+    String input;
+
+    @Option(name = "-output", metaVar = "[path]", required = true, usage = "output path")
+    String output;
+
+    @Option(name = "-reducers", metaVar = "[num]", usage = "number of reducers")
+    int numReducers = 1;
+  }
+
+  /**
+   * Runs this tool.
+   */
+  @Override
+  public int run(String[] argv) throws Exception {
+    final Args args = new Args();
+    CmdLineParser parser = new CmdLineParser(args, ParserProperties.defaults().withUsageWidth(100));
+
+    try {
+      parser.parseArgument(argv);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      return -1;
+    }
+
+    LOG.info("Tool: " + ComputeMeanV1.class.getSimpleName());
+    LOG.info(" - input path: " + args.input);
+    LOG.info(" - output path: " + args.output);
+    LOG.info(" - number of reducers: " + args.numReducers);
+
+    Configuration conf = getConf();
+    Job job = Job.getInstance(conf);
+    job.setJobName(ComputeMeanV1.class.getSimpleName());
+    job.setJarByClass(ComputeMeanV1.class);
+
+    job.setNumReduceTasks(args.numReducers);
+
+    FileInputFormat.setInputPaths(job, new Path(args.input));
+    FileOutputFormat.setOutputPath(job, new Path(args.output));
+
+    job.setInputFormatClass(KeyValueTextInputFormat.class);
+    job.getConfiguration().set("mapreduce.input.keyvaluelinerecordreader.key.value.separator", "\t");
+    job.setMapOutputKeyClass(Text.class);
+    job.setMapOutputValueClass(PairOfInts.class);
+    job.setOutputKeyClass(Text.class);
+    job.setOutputValueClass(IntWritable.class);
+    job.setOutputFormatClass(TextOutputFormat.class);
+
+    job.setMapperClass(MyMapper.class);
+    job.setCombinerClass(MyCombiner.class);
+    job.setReducerClass(MyReducer.class);
+
+    // Delete the output directory if it exists already.
+    Path outputDir = new Path(args.output);
+    FileSystem.get(conf).delete(outputDir, true);
+
+    long startTime = System.currentTimeMillis();
+    job.waitForCompletion(true);
+    LOG.info("Job Finished in " + (System.currentTimeMillis() - startTime) / 1000.0 + " seconds");
+
+    return 0;
+  }
+
+  /**
+   * Dispatches command-line arguments to the tool via the {@code ToolRunner}.
+   */
+  public static void main(String[] args) throws Exception {
+    ToolRunner.run(new ComputeMeanV4(), args);
+  }
+}
