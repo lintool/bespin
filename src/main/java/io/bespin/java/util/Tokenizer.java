17a9f0f4ed3d629b1a545d06f3bd021de2cf46bc
lintool <jimmylin@uwaterloo.ca>
2017-01-08 07:51:48 -0500

    Pre-compile regular expressions.

--- src/main/java/io/bespin/java/util/Tokenizer.java
+++ src/main/java/io/bespin/java/util/Tokenizer.java
@@ -19,16 +19,19 @@ package io.bespin.java.util;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
+import java.util.regex.Pattern;
 
 /**
  * Tokenizer so that we have a consistent definition of a "word".
  */
 public class Tokenizer {
+  private static final Pattern PATTERN = Pattern.compile("(^[^a-z]+|[^a-z]+$)");
+
   public static List<String> tokenize(String input) {
     List<String> tokens = new ArrayList<>();
     StringTokenizer itr = new StringTokenizer(input);
     while (itr.hasMoreTokens()) {
-      String w = itr.nextToken().toLowerCase().replaceAll("(^[^a-z]+|[^a-z]+$)", "");
+      String w = PATTERN.matcher(itr.nextToken().toLowerCase()).replaceAll("");
       if (w.length() != 0) {
         tokens.add(w);
       }
--- src/main/scala/io/bespin/scala/util/Tokenizer.scala
+++ src/main/scala/io/bespin/scala/util/Tokenizer.scala
@@ -22,8 +22,10 @@ import scala.collection.JavaConverters._
 
 trait Tokenizer {
   def tokenize(s: String): List[String] = {
+    val pattern = """(^[^a-z]+|[^a-z]+$)""".r
+
     new StringTokenizer(s).asScala.toList
-      .map(_.asInstanceOf[String].toLowerCase().replaceAll("(^[^a-z]+|[^a-z]+$)", ""))
+      .map(t => pattern.replaceAllIn(t.asInstanceOf[String].toLowerCase(), ""))
       .filter(_.length != 0)
   }
 }
